eb6c74ed02fbb9667d053a5996ea90b56b02a2d5
anhnguyendon92 <anhnguyendon92@gmail.com>
2021-03-20 20:27:13 -0700

    API and basic jsx

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Heading from './componets/heading';
+import Form from './componets/form';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//const api_key = '0f3e11117cf4815f182186649c2179fa';
+
+
+class App extends React.Component() {
+    state = {
+        temperture: '',
+        city: '',
+        country: '',
+        humidity: '',
+        pressure: '',
+        icon: '',
+        description: '',
+        error: ''
+    }
+    render() {
+        return(
+            <div>
+            <Heading />
+            <Form />
+            </div>
+        )
+    }
+   
 }
 
 export default App;
